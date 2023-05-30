13d83b8029f43345aba539efcf69b9cb0c0033d5
Lorne Cyr <Lorne.S.Cyr@gmail.com>
2023-05-29 21:05:49 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My React Portfolio</h1>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
