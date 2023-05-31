a89a2e9ebe10dc2d6e58f6adc6555d2c7e243560
Lorne Cyr <Lorne.S.Cyr@gmail.com>
2023-05-31 19:21:48 -0300

    Added navigation and styling. Blocked out some components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
+import { useState } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
 
 function App() {
+  const [activeSection, setActiveSection] = useState("about-me");
+
+  const changeActiveSection = (targetState) => {
+    setActiveSection(targetState);
+  };
+
+  const mainSection = () => {
+    if (activeSection === "about-me") {
+      return <About />;
+    } else if (activeSection === "portfolio") {
+      return <Portfolio />;
+    } else if (activeSection === "contact-me") {
+      return <Contact />;
+    }
+  };
+
   return (
     <div className="App">
-      <Header />
-      <main>
-        <About />
-      </main>
+      <Header changeActiveSection={changeActiveSection} />
+      <main>{mainSection()}</main>
       <Footer />
     </div>
   );
