17df3ada3441b769a50c397f81af1b1851a77e34
Lorne Cyr <Lorne.S.Cyr@gmail.com>
2023-06-01 21:25:20 -0300

    Built and styles resume component

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,7 +4,9 @@ import styles from "./Footer.module.css";
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      <div className={styles.container}>This is the Footer</div>
+      <div className={styles.container}>
+        © 2023 All rights reserved. – Crafted & Coded by Lorne Cyr
+      </div>
     </div>
   );
 };
