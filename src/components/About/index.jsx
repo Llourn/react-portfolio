2ef3eb12b8b1ffb552c3ed2c332108243e054873
Lorne Cyr <Lorne.S.Cyr@gmail.com>
2023-06-11 16:41:54 -0300

    Added avatar to about component.

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./About.module.css";
+import lorneImage from "./working-hard.jpeg";
 import { FaTwitterSquare, FaGithubSquare, FaLinkedin } from "react-icons/fa";
 
 const About = () => {
@@ -17,53 +18,63 @@ const About = () => {
       <section className={styles.infoSection}>
         <h1>About Me</h1>
         <section>
-          <p className={styles.avatar}>(;-_-)ノ</p>
-          <p>Hi, I'm Lorne.</p>
-          <p>
-            I'm a software developer based in Fredericton, NB specializing in
-            full stack development. I appreciate motivation, but prefer
-            discipline especially when it comes to learning and writing code. My
-            career in development started in gameplay programming, building
-            games/applications for studios throughout the maritimes. I've since
-            moved into web development, building online courses for{" "}
-            <a href="http://ufred.ca" target="_blank" rel="noopener noreferrer">
-              The University of Fredericton
-            </a>{" "}
-            and Vue apps for{" "}
-            <a
-              href="http://battlesnake.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Battlesnake Inc.
-            </a>{" "}
-            In my spare time I like to work on personal projects related to
-            other interests of mine, like learning Japanese 「がんばってね！」.
-            I also value work/life balance and know when to step away from the
-            computer and recharge.
-          </p>
-          <div className={styles.socials}>
-            <a
-              href="https://www.twitter.com/llourn"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaTwitterSquare />
-            </a>
-            <a
-              href="https://github.com/Llourn"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaGithubSquare />
-            </a>
-            <a
-              href="https://www.linkedin.com/in/lornecyr/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaLinkedin />
-            </a>
+          <img
+            className={styles.avatar}
+            src={lorneImage}
+            alt="Lorne Cyr, diligently working on something super important."
+          />
+          <div className={styles.content}>
+            <p>Hi, I'm Lorne.</p>
+            <p>
+              I'm a software developer based in Fredericton, NB specializing in
+              full stack development. I appreciate motivation, but prefer
+              discipline especially when it comes to learning and writing code.
+              My career in development started in gameplay programming, building
+              games/applications for studios throughout the maritimes. I've
+              since moved into web development, building online courses for{" "}
+              <a
+                href="http://ufred.ca"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                The University of Fredericton
+              </a>{" "}
+              and Vue apps for{" "}
+              <a
+                href="http://battlesnake.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Battlesnake Inc.
+              </a>{" "}
+              In my spare time I like to work on personal projects related to
+              other interests of mine, like learning Japanese
+              「がんばってね！」. I also value work/life balance and know when
+              to step away from the computer and recharge.
+            </p>
+            <div className={styles.socials}>
+              <a
+                href="https://www.twitter.com/llourn"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaTwitterSquare />
+              </a>
+              <a
+                href="https://github.com/Llourn"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaGithubSquare />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/lornecyr/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaLinkedin />
+              </a>
+            </div>
           </div>
         </section>
       </section>
